de873e4a416be8234df8128bc78b258f83fc576c
disnetlab <disnetlab@gmail.com>
2019-04-04 00:34:59 +0000

    initial deployment fix

--- BrownoutPrototype/src/brownout/ComposeFileExecution.java
+++ BrownoutPrototype/src/brownout/ComposeFileExecution.java
@@ -7,15 +7,17 @@ package brownout;
  */
 public class ComposeFileExecution extends CommandExecution{
 
+	String remote_connection = " ssh ubuntu@192.168.0.120 ";
+
 	public void initialDeploymentByComposeFile(){
 		System.out.println("######Initial Deployment From Compose File######");
-		String command = "sh initialDeployment.sh";
+		String command = remote_connection + "sh scripts/initialDeployment.sh";
 		executeCommandWithLessInfo(command);
 	}
 	
 	public void updateDeploymentByComposeFile(){
 		System.out.println("######Update Deployment From Compose File######");
-		String command = "sh updateDeployment.sh";
+		String command = remote_connection + "sh scripts/updateDeployment.sh";
 		executeCommandWithLessInfo(command);
 	}
 }
--- BrownoutPrototype/src/updateDeployment.sh
+++ BrownoutPrototype/src/updateDeployment.sh
@@ -1,4 +1,2 @@
-
-
 #!/bin/sh
 sudo docker stack deploy --compose-file v3-docker-compose-brownout.yml sockshop
